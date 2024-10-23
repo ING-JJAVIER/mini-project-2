b201949abc823640013cf69bdf24cca319501943
Jose Javier <ing.josemartinezmendez@gmail.com>
2024-10-23 07:05:04 -0600

    change in class to className for some elements

--- components/barPorcent.jsx
+++ components/barPorcent.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function BarPorcent({}) {
     return (
         <div className='w-full'>
-            <div class="w-full bg-gray-200 rounded-full h-3 mb-4 dark:bg-gray-700">
-                <div class="bg-blue-600 h-3 text-[.5rem] font-medium text-blue-500 text-center p-0.5 leading-none rounded-full dark:bg-[#ffec65]" style={{width: '51%'}}>51%</div>
+            <div className="w-full bg-gray-200 rounded-full h-3 mb-4 dark:bg-gray-700">
+                <div className="bg-blue-600 h-3 text-[.5rem] font-medium text-blue-500 text-center p-0.5 leading-none rounded-full dark:bg-[#ffec65]" style={{width: '51%'}}>51%</div>
             </div>
         </div>
     )
